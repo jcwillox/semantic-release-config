54f94912aa10500c19f0274e92acc423f83007fd
Josh Willox <joshwillox@gmail.com>
2023-11-13 09:36:12 +1100

    refactor: use built in types from semantic-release

--- src/plugins/exec-cmd.ts
+++ src/plugins/exec-cmd.ts
@@ -1,11 +1,11 @@
 import SemanticReleaseError from "@semantic-release/error";
-import { Config, Context } from "semantic-release";
-import { defineHook } from "../utils";
+import type { VerifyConditionsContext } from "semantic-release";
+import { definePluginHooks } from "../utils";
 import { isExecaError, runCommand } from "../utils/run-command";
 
 const runFailCommand = async (
   cmd: string,
-  ctx: Context & Config,
+  ctx: VerifyConditionsContext,
   errorName: string,
 ) => {
   ctx.logger.log('Running command "%s"', cmd);
@@ -20,32 +20,30 @@ const runFailCommand = async (
   }
 };
 
-export const verifyConditions = defineHook(async (_, ctx) => {
-  const cmd = ctx.env.SEMANTIC_RELEASE_CMD_VERIFY;
-  if (!cmd) return;
-  return cmd && runFailCommand(cmd, ctx, "EVERIFYCONDITIONS");
-});
-
-export const generateNotes = defineHook(async (_, ctx) => {
-  const cmd = ctx.env.SEMANTIC_RELEASE_CMD_GENERATE_NOTES;
-  if (!cmd) return;
-  return cmd && runFailCommand(cmd, ctx, "EGENERATENOTES");
-});
-
-export const prepare = defineHook(async (_, ctx) => {
-  const cmd = ctx.env.SEMANTIC_RELEASE_CMD_PREPARE;
-  if (!cmd) return;
-  return cmd && runFailCommand(cmd, ctx, "EPREPAREFAILED");
-});
-
-export const publish = defineHook(async (_, ctx) => {
-  const cmd = ctx.env.SEMANTIC_RELEASE_CMD_PUBLISH;
-  if (!cmd) return;
-  return cmd && runFailCommand(cmd, ctx, "EPUBLISHFAILED");
-});
-
-export const addChannel = defineHook(async (_, ctx) => {
-  const cmd = ctx.env.SEMANTIC_RELEASE_CMD_CHANNEL;
-  if (!cmd) return;
-  return cmd && runFailCommand(cmd, ctx, "EPUBLISHFAILED");
+export default definePluginHooks({
+  verifyConditions: async (_, ctx) => {
+    const cmd = ctx.env.SEMANTIC_RELEASE_CMD_VERIFY;
+    if (!cmd) return;
+    return cmd && runFailCommand(cmd, ctx, "EVERIFYCONDITIONS");
+  },
+  generateNotes: async (_, ctx) => {
+    const cmd = ctx.env.SEMANTIC_RELEASE_CMD_GENERATE_NOTES;
+    if (!cmd) return;
+    return cmd && runFailCommand(cmd, ctx, "EGENERATENOTES");
+  },
+  prepare: async (_, ctx) => {
+    const cmd = ctx.env.SEMANTIC_RELEASE_CMD_PREPARE;
+    if (!cmd) return;
+    return cmd && runFailCommand(cmd, ctx, "EPREPAREFAILED");
+  },
+  publish: async (_, ctx) => {
+    const cmd = ctx.env.SEMANTIC_RELEASE_CMD_PUBLISH;
+    if (!cmd) return;
+    return cmd && runFailCommand(cmd, ctx, "EPUBLISHFAILED");
+  },
+  addChannel: async (_, ctx) => {
+    const cmd = ctx.env.SEMANTIC_RELEASE_CMD_CHANNEL;
+    if (!cmd) return;
+    return cmd && runFailCommand(cmd, ctx, "EPUBLISHFAILED");
+  },
 });
